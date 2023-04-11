04b8f455415143e37f6ac80e07b45c3ef909ff63
vkovalev <studentkov@mail.ru>
2023-04-11 13:51:40 +0700

    feat: add header in TS

--- front/src/index.ts
+++ front/src/index.ts
@@ -1,11 +1,10 @@
+ /* eslint-disable */ 
 import './_global.scss';
 import '../index.html';
 import './components/_add_favicon';
-// import './components/_CreateBaseData';
-// import './components/_ModelCreateFilterData'
 import './components/_add_rangeSlider';
-import ViewMain from './components/view/_ViewMain'
+import ViewMain from './components/view/_ViewMain';
+import Header from './components/view/_ViewHeader';
 
-const App = new ViewMain()
+const header = new Header();
 
-App.init()
