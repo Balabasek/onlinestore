b990f6646df29f27abf460cd3b51ebc02ed66fc4
vkovalev <studentkov@mail.ru>
2023-04-11 13:28:44 +0700

    created custom element

---
+++ front/src/components/view/_ViewMain.ts
@@ -0,0 +1,63 @@
+import ControllerMain from "../controller/_ControllerMain";
+import Router from "../router";
+
+class ViewMain {
+  _controller: ControllerMain;
+  _router: Router;
+  root: HTMLElement;
+
+  constructor() {
+    this.root = document.body;
+    this._controller = new ControllerMain();
+    this._router = new Router()
+
+    this._router.startRouteListenner()
+    this._router.handleLocation()
+
+    // this._controller.router.startRouteListenner()
+    // console.log("this._controller.router.count",this._controller.router.count)
+  }
+
+  createFooter() {
+
+    const footer: HTMLElement = document.createElement("footer")
+
+    footer.innerHTML = `<footer class="page-footer _main-container">
+  <section class="footer _container">
+    <div class="footer__item github">
+      <div class="footer__link-container">
+        <div class="footer__link-container-item">
+          <a href="https://github.com/Balabasek" class="footer__link footer__github"></a>
+          <p class="footer__author">Balabasek</p>
+        </div>
+        <div class="footer__link-container-item">
+          <a href="https://github.com/LjeDmitriy" class="footer__link footer__github"></a>
+          <p class="footer__author">LjeDmitriy</p>
+        </div>
+      </div>
+    </div>
+    <p class="footer__item date-relese">2023</p>
+    <div class="footer__item sibsutis">
+      <a href="https://sibsutis.ru/" class="footer__link footer__sibsutis"></a>
+    </div>
+  </section>
+</footer>`
+
+footer.onclick = () => {this._router.pushState('/product')}
+
+    this.root.append(footer)
+  }
+
+  createMain() {
+    console.log("1")
+  }
+
+  init() {
+    this.createFooter()
+    // this._controller.router.startRouteListenner()
+
+  }
+
+}
+
+export default ViewMain
--- front/src/index.ts
+++ front/src/index.ts
@@ -1,9 +1,10 @@
 import './_global.scss';
 import '../index.html';
 import './components/_add_favicon';
+// import './components/_CreateBaseData';
+// import './components/_ModelCreateFilterData'
 import './components/_add_rangeSlider';
-
-import ViewMain from './components/_ViewMain'
+import ViewMain from './components/view/_ViewMain'
 
 const App = new ViewMain()
 
