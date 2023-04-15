d3ca7386eb79996bf2906dc778288c9e3de8b0e2
vkovalev <studentkov@mail.ru>
2023-04-16 00:23:44 +0700

    refactor: main, footer, header and feat: const, biewBlocks, utils

--- front/src/components/view/_ViewFooter.ts
+++ front/src/components/view/_ViewFooter.ts
@@ -1,6 +1,7 @@
 import ControllerMain from "../controller/_ControllerMain";
 import { stringArrayObject } from '../typingTS/_type'
 import Router from "../router";
+import { FOOTER } from '../utils/const';
 
 class ViewFooter {
     _controller: ControllerMain;
@@ -27,33 +28,28 @@ class ViewFooter {
     }
 
     createFooter() {
-
-        const footer: HTMLElement = document.createElement("footer")
-
-        footer.innerHTML = `<footer class="page-footer _main-container">
+        FOOTER.innerHTML = `<footer class="page-footer _main-container">
   <section class="footer _container">
-    <div class="footer__item github">
-      <div class="footer__link-container">
-        <div class="footer__link-container-item">
-           <a href="https://github.com/Balabasek" class="footer__link footer__github"></a>
-           <p class="footer__author">Balabasek</p>
+        <div class="footer__item github">
+            <div class="footer__link-container">
+                <div class="footer__link-container-item">
+                    <a href="https://github.com/Balabasek" class="footer__link footer__github"></a>
+                    <p class="footer__author">Balabasek</p>
+                </div>
+                <div class="footer__link-container-item">
+                    <a href="https://github.com/LjeDmitriy" class="footer__link footer__github"></a>
+                    <p class="footer__author">LjeDmitriy</p>
+                </div>
+            </div>
         </div>
-        <div class="footer__link-container-item">
-           <a href="https://github.com/LjeDmitriy" class="footer__link footer__github"></a>
-           <p class="footer__author">LjeDmitriy</p>
-        </div>
-      </div>
-    </div>
-    <p class="footer__item date-relese">2023</p>
-    <div class="footer__item sibsutis">
+        <p class="footer__item date-relese">2023</p>
+        <div class="footer__item sibsutis">
             <a href="https://sibsutis.ru/" class="footer__link footer__sibsutis"></a>
-    </div>
+        </div>
   </section>
 </footer>`
 
-        footer.onclick = () => { this._router.pushState('/product') }
-
-        this.root.append(footer)
+        FOOTER.onclick = () => { this._router.pushState('/product') }
     }
 
     createMain() {
