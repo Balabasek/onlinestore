9615f031a13a7a65a0e3df3b2a1c5c4ce97a6fd9
vkovalev <studentkov@mail.ru>
2023-04-16 23:01:30 +0700

    fix: Modelmetods

--- front/src/components/typingTS/_interfaces.ts
+++ front/src/components/typingTS/_interfaces.ts
@@ -1,5 +1,7 @@
 import { stringObject} from './_type'
 
+import { stringObject} from './_type'
+
 interface IitemDATA {
     id: number;
     title: string;
@@ -12,67 +14,16 @@ interface IitemDATA {
     category: string;
     thumbnail: string;
     images: string[];
-  }
+}
 
 
-  interface IFilter {
+interface IFilter {
     category: string[];
     brand: string[];
     price: number[];
     stock: number[];
-    search: string[];
+    search: string;
 }
 
 
 export { IitemDATA, IFilter}
-
-
-// import { option } from './_type'
-
-// interface ISource {
-//     category: string;
-//     country: string;
-//     description: string;
-//     id: string;
-//     language: string;
-//     name: string;
-//     url: string;
-// }
-
-// interface INews {
-//     author: string;
-//     content: string;
-//     description: string;
-//     publishedAt: string;
-//     source: {
-//         id: string,
-//         name: string
-//     };
-//     title: string;
-//     url: string;
-//     urlToImage: string;
-// }
-
-// interface IGetResp {
-//     endpoint: string;
-//     options?: Partial<option>;
-// }
-
-
-// interface IDrawSources {
-//     status: string;
-//     sources: ISource[];
-// }
-
-// interface IdrawNews {
-//     status: string;
-//     totalResults: number;
-//     articles: INews[];
-
-// }
-
-// interface IdrawNewsError {
-//     status: string;
-//     code: string;
-//     message: string;
-// }
