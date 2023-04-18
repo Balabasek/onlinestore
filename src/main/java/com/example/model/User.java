417c50ed660dbb14a03f5151442f3bd126b47d9f
dstatsenko <dimastacenko104@gmail.com>
2023-04-18 19:11:05 +0700

    Create Item and User entity

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,74 @@
+package com.example.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    @Id
+    private final String id;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String secondName;
+
+    private String login;
+
+    private String password;
+
+    public User(String id, String firstName, String middleName, String secondName, String login, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.secondName = secondName;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
