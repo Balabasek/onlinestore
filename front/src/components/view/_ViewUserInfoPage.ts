8a5e220bd567f83e2e360cf9b2e360962333b831
vkovalev <studentkov@mail.ru>
2023-05-13 00:12:51 +0700

    feat: add logout + userinfoPage

--- front/src/components/view/_ViewUserInfoPage.ts
+++ front/src/components/view/_ViewUserInfoPage.ts
@@ -2,20 +2,41 @@ import CustomElement from '../utils/_createCustomElement';
 
 class ViewUserInfoPage {
     customElement: CustomElement
+    nickname: HTMLElement
+    EVENT: { [x: string]: Event };
 
     constructor() {
         this.customElement = new CustomElement();
+        this.nickname = this.customElement.createElement('span', { className: 'userinfo__nickname', textContent: 'unknown' });
+
+        this.EVENT = {
+            clickLogout: new Event('clickLogout', { bubbles: true }),
+        };
     }
 
-    create() {
-        //Header контейнер
-        const userInfoContainer = this.customElement.createElement('section', {className: 'userInfo _container'});
+    create(stringPromise: string) {
 
-        const userInfoText = this.customElement.createElement('h2', {className: 'userinfo__login', textContent: 'Your login:' });
+        const userInfoContainer = this.customElement.createElement('section', {className: 'userinfo__container'});
 
-        this.customElement.addChildren(userInfoContainer, [userInfoText])
+        const userInfoText = this.customElement.createElement('h2', {className: 'userinfo__text', textContent: 'Your login:' });
+        const logout = this.customElement.createElement('button', { className: 'logout_btn', textContent: 'Logout' });
 
-        return userInfoContainer
+        logout.addEventListener('click', () => {
+            logout.dispatchEvent(this.EVENT.clickLogout);
+        })
+
+        const userNicknameText = this.customElement.createElement('p', {className: 'userinfo__nickname' });
+        this.setNickname(stringPromise)
+        this.customElement.addChildren(userNicknameText, [this.nickname])
+
+
+        this.customElement.addChildren(userInfoContainer, [userInfoText, userNicknameText, logout]);
+
+        return userInfoContainer;
+    }
+
+    async setNickname(stringPromise: string) {
+        this.nickname.textContent = await stringPromise;
     }
 }
 export default ViewUserInfoPage;
